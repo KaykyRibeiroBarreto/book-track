4041ad92a8699c129ca165111f274f9061f80efc
Kayky Ribeiro Barreto <kaykyribeiro207@gmail.com>
2025-04-24 17:47:16 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+        <h1>Book Track</h1>
+        <button className='addButton'> ADICIONAR LIVRO</button>
+    </header>
+  )
+}
+
+export default Header
