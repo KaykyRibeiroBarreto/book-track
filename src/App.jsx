db27230f80b35620c141973fc3cfb85ad2b91820
Kayky Ribeiro Barreto <kaykyribeiro207@gmail.com>
2025-04-25 18:09:26 -0300

    feat(app): add EmptyState component and display logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import EmptyState from './components/EmptyState'
 
 function App() {
+  const [livros, setLivros] = useState([])
+
   return (
     <>
       <Header/>
-    </>
+      {livros.length === 0 // Se o array de livros estiver vazio
+        ? <EmptyState /> // Mostra o componente EmptyState
+        : <BooksGrid livros={livros} /> // Se não, mostra o componente BooksGrid
+      }   
+   </>
   )
 }
 
---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,12 @@
+    import React from 'react'
+    
+    const EmptyState = () => {
+      return (
+        <div>
+          <h2>Nenhum livro ainda...</h2>
+          <p>Adicione alguns livros para começar.</p> 
+        </div>
+      )
+    }
+    
+    export default EmptyState
