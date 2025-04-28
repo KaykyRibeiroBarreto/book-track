6720c5bf2691c4e14087d2e85941708d51a1e1a1
Kayky Ribeiro <142861320+KaykyRibeiroBarreto@users.noreply.github.com>
2025-04-28 14:59:30 -0300

    feat: implement EmptyState component with AddBookCard integration

---
+++ src/components/AddBookCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddBookCard = () => {
+  return (
+    <div>
+      <form>
+        <label>Enter your name:
+            <input type="text" />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default AddBookCard
--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,13 +1,25 @@
     import React from 'react'
     import styles from './EmptyState.module.css';
+    import AddBookCard from './AddBookCard';
+    import emptyBook from '../assets/empty-book.png';
 
     const EmptyState = () => {
+      const [isAddingBook, setIsAddingBook] = React.useState(false);
+
       return (
         <div className={styles.emptyState}>
-          <img src={"/assets/empty.svg"} alt="Nenhum livro encontrado" className={styles.image} />
+          {isAddingBook ? (  // se isAddingBook for true, renderiza the AddBookCard component 
+            <AddBookCard/>   // se não renderiza conteúdo padrão
+          ) : null}
+
+          <img 
+          src={emptyBook}
+          alt="Empty Book"
+          style={{ width: '150px', height: '150px' }}
+          />
           <h2 className={styles.title}>Nenhum livro ainda...</h2>
           <p className={styles.text}>Adicione alguns livros para começar.</p> 
-          <button className={styles.addButton}>Adicionar Livro</button>
+          <button className={styles.addButton} onClick={()=> setisAddingBook(true)}>Adicionar Livro</button>
 
         </div>
       )
