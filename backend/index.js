5552732d57bc7041b0b09e8f57b1d323728f8a51
Kayky Ribeiro Barreto <kaykyribeiro207@gmail.com>
2025-05-14 18:08:51 -0300

    feat: implement routes to display books and welcome message

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,31 @@
 import express from 'express';
+
+const app = express();
+const livros = [
+    {
+        id: 1,
+        nome: 'O Senhor dos Anéis',
+        autor: 'J.R.R. Tolkien',
+        ano: 1954
+    },
+    {
+        id: 2,
+        nome: '1984',
+        autor: 'George Orwell',
+        ano: 1949
+    },
+    {
+        id: 3,
+        nome: 'O Hobbit',
+        autor: 'J.R.R. Tolkien',
+        ano: 1937
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+})
