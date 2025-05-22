a99ad7516c8c7b9bd07d7e9ad089fcd419056bb1
Kayky Ribeiro Barreto <kaykyribeiro207@gmail.com>
2025-05-22 13:38:14 -0300

    feat: remove redundant GET /livros route and improve PUT /livros/:id implementation

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -11,13 +11,10 @@ const livros = [
     { id: 3, nome: 'O Hobbit', autor: 'J.R.R. Tolkien', ano: 1937 }
 ];
 
+
 // Rotas
 app.get('/', (req, res) => {
     res.send('Hello World!');
-});
-
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
 });
 
 app.get('/livros/:id', (req, res) => {
@@ -31,9 +28,29 @@ app.get('/livros/:id', (req, res) => {
     res.status(200).json(livro);
 });
 
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro adicionado com sucesso');
+})
+
+//rota PUT
+app.put('/livros/:id', (req, res) => {
+    const id = parseInt(req.params.id); // pega o id da URL e converte para número
+    const index = livros.findIndex(l => l.id === id) // Procura o índice do livro no array pelo id
+
+    if (index === -1) {
+        return res.status(404).json({ erro: 'Livro não encontrado'})
+    }
+
+    livros[index] = {...livros[index], ...req.body}
+    res.status(200).json(livros[index]);
+})
+
+app.delete('/livros/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    livros.splice(livros.findIndex(l => l.id === id), 1); // Remove o livro do array
+    res.status(200).json({ mensagem: 'Livro removido com sucesso' });
 })
 
 // Inicia o servidor
